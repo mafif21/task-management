85cf8ba62829cdad433936de3588491ea74288c5
Muhammad Nurul Afif Maliki <afif.maliki21@gmail.com>
2023-08-04 00:49:04 +0700

    checking node server

--- server/index.js
+++ server/index.js
@@ -1,2 +1,10 @@
-console.log("hello world");
-console.log("test lur");
+import express from "express";
+
+const app = express();
+const port = 4000;
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(port, () => console.log("hello world"));
