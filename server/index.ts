8d9cb883ba29adba96278ddb110d76d87e7a547b
Muhammad Nurul Afif Maliki <afif.maliki21@gmail.com>
2023-08-07 23:48:15 +0700

    setup cors and body parser

--- server/index.ts
+++ server/index.ts
@@ -5,9 +5,17 @@ import express, {
 } from 'express';
 import { DataSource } from 'typeorm';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
 const app: Express = express();
 dotenv.config();
+
+// parse request body
+app.use(bodyParser.json());
+
+// allow port access to another port
+app.use(cors());
 
 // PORT
 const port = 4000 || process.env.PORT;
